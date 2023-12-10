a010a453f958c4b4cb997367d337c6fb3bc893f9
Maxime Mangel <mangel.maxime@protonmail.com>
2023-12-10 23:19:34 +0100

    Try fix windows CI

--- src/eleventyComputed/page/absolutePath.ts
+++ src/eleventyComputed/page/absolutePath.ts
@@ -4,7 +4,7 @@ export default function absolutePath() {
     return (data: any) => {
         // Work around Eleventy bug/limitation when run via code and not CLI
         // See: https://github.com/11ty/eleventy/issues/2793
-        return path.join(process.cwd(), data.page.inputPath);
+        return path.normalize(path.join(process.cwd(), data.page.inputPath));
         // It would be better to use this, but it doesn’t work
         // return path.join(data.eleventy.env.root, data.page.inputPath);
     };
